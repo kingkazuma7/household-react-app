fa40a2248be6687cc2f15a72945f1211fb9284b9
kingkazuma7 <your.nasionasio7@gmail.com>
2024-09-06 15:27:20 +0900

    11 レイアウトファイルの構造

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -21,16 +21,8 @@ import { Outlet } from 'react-router-dom'
 
 const drawerWidth = 240;
 
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * Remove this when copying and pasting into your project.
-   */
-  window?: () => Window;
-}
 
-export default function ResponsiveDrawer(props: Props) {
-  const { window } = props;
+export default function AppLayout() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [isClosing, setIsClosing] = React.useState(false);
 
@@ -81,12 +73,10 @@ export default function ResponsiveDrawer(props: Props) {
     </div>
   );
 
-  // Remove this const when copying and pasting into your project.
-  const container = window !== undefined ? () => window().document.body : undefined;
-
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: 'flex', bgcolor: (theme) => theme.palette.grey[100], minHeight: '100vh' }}>
       <CssBaseline />
+      {/* ヘッダー */}
       <AppBar
         position="fixed"
         sx={{
@@ -109,14 +99,15 @@ export default function ResponsiveDrawer(props: Props) {
           </Typography>
         </Toolbar>
       </AppBar>
+      {/* サイドバー */}
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         aria-label="mailbox folders"
       >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        {/* 実装はSEOの重複を避けるためにJavaScriptで置き換えることができます。 */}
+        {/* SP */}
         <Drawer
-          container={container}
           variant="temporary"
           open={mobileOpen}
           onTransitionEnd={handleDrawerTransitionEnd}
@@ -131,6 +122,8 @@ export default function ResponsiveDrawer(props: Props) {
         >
           {drawer}
         </Drawer>
+
+        {/* PC */}
         <Drawer
           variant="permanent"
           sx={{
@@ -142,6 +135,7 @@ export default function ResponsiveDrawer(props: Props) {
           {drawer}
         </Drawer>
       </Box>
+      {/* メインコンテンツ */}
       <Box
         component="main"
         sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
