8ac60943bc15afa2ed0d4e9f52fa0126806014c9
kingkazuma7 <your.nasionasio7@gmail.com>
2024-09-09 09:18:19 +0900

    16 theme スタイルを設定

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,22 @@ import Home from './pages/Home';
 import Report from './pages/Report';
 import Nomatch from './pages/Nomatch';
 import AppLayout from './components/layout/AppLayout';
+import { theme } from './theme/theme';
+import { ThemeProvider } from '@mui/material';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AppLayout/>}>
-          <Route index element={<Home />} />
-          <Route path="/report" element={<Report />} />
-          <Route path="*" element={<Nomatch />} />
-        </Route>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<AppLayout/>}>
+            <Route index element={<Home />} />
+            <Route path="/report" element={<Report />} />
+            <Route path="*" element={<Nomatch />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
